149d5ee86ed9f8ade1495fb63394337a7a811d87
Parker Emerson <parker.emerson@gmail.com>
2018-10-14 16:04:59 -0700

    Rollable tables and broken popovers

--- content/javascript/script.js
+++ content/javascript/script.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
     $("#nav-placeholder").load("https://emersonp.github.io/nav.html");
+    $("[data-toggle=popover]").popover();
 });
+
+var rollOnTable = function(tableid) {
+    $('#' + tableid + ' tr').removeClass('rolled');
+    var rowCount = document.getElementById(tableid).rows.length;
+    var row = Math.floor(Math.random() * (rowCount - 1) + 1);
+    $('#' + tableid + ' tr').eq(row).addClass('rolled');
+};

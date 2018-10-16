08636e379fbbebc73ba16bd2fd301905305b8684
Parker Emerson <parker.emerson@gmail.com>
2018-10-16 13:57:07 -0400

    Collapsing sections

--- content/javascript/script.js
+++ content/javascript/script.js
@@ -7,8 +7,19 @@ $( document ).ready(function() {
 });
 
 var rollOnTable = function(tableid) {
+    console.log("tableid: " + tableid);
     $('#' + tableid + ' tr').removeClass('rolled');
     var rowCount = document.getElementById(tableid).rows.length;
     var row = Math.floor(Math.random() * (rowCount - 1) + 1);
+    console.log("rowCount: " + rowCount);
+    console.log("row: " + row);
     $('#' + tableid + ' tr').eq(row).addClass('rolled');
 };
+
+var toggleCollapseArrow = function(tableid) {
+    if($('#div-' + tableid).hasClass('show')) {
+        $('#btn-' + tableid).text('+');
+    } else {
+        $('#btn-' + tableid).html('&ndash;');
+    }
+}

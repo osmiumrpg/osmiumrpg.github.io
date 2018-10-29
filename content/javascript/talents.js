95c1f34a0257cab27ec0ad0a4679c8a610639178
Parker Emerson <parker.emerson@gmail.com>
2018-10-29 11:03:04 -0700

    Safety checkin

--- content/javascript/talents.js
+++ content/javascript/talents.js
@@ -23,10 +23,10 @@ var filter_talents = function() {
         var tierA = a.Tier;
         var tierB = b.Tier;
 
-        if (nameA < nameB) return -1;
-        if (nameA > nameB) return 1;
         if (tierA < tierB) return -1;
         if (tierA > tierB) return 1;
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
         return 0;
     });
 
@@ -34,7 +34,7 @@ var filter_talents = function() {
     filtered_talents.forEach(function(element) {
         $('#talents').append("<h6>" + element.Name + "</h6><div class='talent-stat'><p><strong>Tier:</strong> <span>" + element.Tier + 
         "</span></p><p><strong>Activation: </strong><span>" + element.Activation + "</span></p><p><strong>Ranked: </strong><span>" + element.Ranked + 
-        "</span></p><p><strong>Tags: </strong><span>" + element.Tags.join(', ') + "</span></sp></div><p>" + element.Text + "</p>");
+        "</span></p><p><strong>Tags: </strong><span>" + element.Tags.join(', ') + "</span></sp></div>" + element.Text);
     });
 
     $('[data-toggle="popover"]').popover({
@@ -247,6 +247,187 @@ var talent_list = [
         Ranked: 'Yes',
         Tags: ['Tough'],
         Text: "<p>Each rank of Grit increases yoru character's strain threshold by one.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Hamstring Shot',
+        Tier: 1,
+        Activation: 'Active (Action)',
+        Ranked: 'No',
+        Tags: ['Ranged'],
+        Text: "<p>Once per round, your character may use this talent to perform a ranged combat check against one non-vehicle target within range of the weapon used. If the check is successful, halve the damage inflicted by the attack (before reducing damage by the target's soak). The target is immobilized until the end of its next turn.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Intuitive Casting',
+        Tier: 1,
+        Activation: 'Passive',
+        Ranked: 'Yes',
+        Tags: ['Magic'],
+        Text: "<p>Your character adds <span class='dice boost'>b</span> for each rank of Intuitive Casting to any magic checks they make against any targets that have not yet taken their turn in the current encounter.</p>",
+        Source: 'CCC-TheSapient'
+    },
+    {
+        Name: 'Jump Up',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'No',
+        Tags: ['Athleticism'],
+        Text: "<p>Once per round during your character's turn, your character may use this talent to stand from a prone or seated position as an incidental.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Knack for It',
+        Tier: 1,
+        Activation: 'Passive',
+        Ranked: 'Yes',
+        Tags: ['Knowledge'],
+        Text: "<p>When you purchase this talent for your character, select one skill. Your character removes <span class='dice setback'>bb</span> from any checks they make using this skill. Each additional time you purchase this talent for your character, select two additional skills. Your character also removes <span class='dice setback'>bb</span> from any checks they make using these skills. You cannot select combat or magic skills when choosing skills for this talent.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Know Somebody',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'No',
+        Tags: ['Social'],
+        Text: "<p>Once per session, when attempting to purchase a legally available item, your character may use this talent to reduce its rarity by one per rank of Know Somebody.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: "Let's Ride",
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'No',
+        Tags: ['Athleticism', 'Vehicles'],
+        Text: "<p>Once per round during your character's turn, your character can use this talent to mount or dismount from a vehicle or animal, or move from one position in a vehicle to another (such as from the cockpit to a gun turret) as an incidental. In addition, if your character suffers a short-range fall (see Core Rules page 112) from a vehicle or animal, they suffer no damage and land on their feet.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'One with Nature',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'No',
+        Tags: ['Adventurer'],
+        Text: "<p>When in the wilderness, your character may make a <strong>Simple (-) Survival check</span>, instead of Discipline or Cool, to recover strain at the end of an encounter (see Core Rules page 117).</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Wheel and Deal',
+        Tier: 1,
+        Activation: 'Passive',
+        Ranked: 'Yes',
+        Tags: ['Social'],
+        Text: "<p>When selling goods legally, gain 10% more currency per rank of Wheel and Deal.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Overchannel',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'Yes',
+        Tags: ['Magic'],
+        Text: "<p>When casting a spell, your character may select a number of additional effect in (<span class='dice difficulty'>d</span>) up to their ranks in Overchannel. The effect must be limited to single <span class='dice difficulty'>d</span> effects. Your character gains <span class='genesys'>hh</span> per <span class='dice difficulty'>d</span> added in this way to the roll, rather than increasing the difficulty of the check. This is in addition to any threat rolled normally. Threat added by this talent can be cancelled by rolled advantages.</p>",
+        Source: 'CCC-ESP77'
+    },
+    {
+        Name: 'Painful Blow',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'Yes',
+        Tags: ['Combat'],
+        Text: "<p>When your character makes a combat check, you may voluntarily increase the difficulty by one to use this talent. If the target suffers one or more wounds from the combat check, the target suffers 2 strain each time they perform a maneuver until the end of the encounter.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Parry',
+        Tier: 1,
+        Activation: 'Active (Incidental, Out of Turn)',
+        Ranked: 'Yes',
+        Tags: ['Melee'],
+        Text: "<p>When your character suffers a hit from a melee combat check, after damage is calculated but before soak is applied (so immediately after Step 3 of Perform a Combat check, Core Rules page 102), your character may suffer 3 strain to use this talent to reduce the damage of the hit by two plus their ranks in Parry. This talent can only be used once per hit, and your character needs to be wielding a Melee weapon.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Physician',
+        Tier: 1,
+        Activation: 'Passive',
+        Ranked: 'Yes',
+        Tags: ['Medicine'],
+        Text: "<p>When making a Medicine check to help a character heal wounds, the target heals 1 additional strain per rank of Physician.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Proper Upbringing',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'Yes',
+        Tags: ['Social'],
+        Text: "<p>When your character makes a social skill check in polite company (as determined by your GM), they may suffer a number of strain to use this talent to add an equal number of <span class='genesys'>a</span> to the check. The number may not exceed your character’s ranks in Proper Upbringing.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Quick Draw',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'No',
+        Tags: ['Melee','Ranged'],
+        Text: "<p>Once per round on your character’s turn, they may use this talent to draw or holster an easily accessible weapon or item as an incidental. Quick Draw also reduces a weapon’s Prepare rating by one, to a minimum of one.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Quick Strike',
+        Tier: 1,
+        Activation: 'Passive',
+        Ranked: 'Yes',
+        Tags: ['Combat'],
+        Text: "<p>Your character adds <span class='dice boost'>b</span> for each rank of Quick Strike to any combat checks they make against any targets that have not yet taken their turn in the current encounter.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Rapid Reaction',
+        Tier: 1,
+        Activation: 'Active (Incidental, Out of Turn)',
+        Ranked: 'Yes',
+        Tags: ['Awareness','Combat'],
+        Text: "<p>Your character may suffer a number of strain to use this talent to add an equal number of <span class='genesys'>s</span> to a Vigilance or Cool check they make to determine Initiative order. The number may not exceed your character’s ranks in Rapid Reaction.</p>",
+        Source: 'GCRB'
+    },
+    {
+        Name: 'Rapid Recovery',
+        Tier: 1,
+        Activation: 'Passive',
+        Ranked: 'Yes',
+        Tags: ['Tough'],
+        Text: "<p>When healing straing after an encounter, heal 1 additional strain per rank of Rapid Recovery.</p>",
+        Source: 'ABGR,ACRB,ECRB,FBGR,FCRB'
+    },
+    {
+        Name: 'Redundant Systems',
+        Tier: 1,
+        Activation: 'Active (Action)',
+        Ranked: 'No',
+        Tags: ['Mechanics','Vehicles'],
+        Text: "<p>Once per session, may take a Redundant Systems action; make an <strong>Easy (<span class='dice difficulty'>d</span>) Mechanics check</span> to harvest components from a functioning device to repair a broken one without breaking the first device.</p>",
+        Source: 'ACRB,ECRB'
+    },
+    {
+        Name: 'Reflect',
+        Tier: 1,
+        Activation: 'Active (Incidental, Out of Tunr)',
+        Ranked: 'Yes',
+        Tags: ['Awareness', 'Melee'],
+        Text: "<p>When making a Medicine check to help a character heal wounds, the target heals 1 additional strain per rank of Physician.</p>",
+        Test: "<p>When your character suffers a hit from a Ranged combat check, after damage is calculated but before soak is applied, your character may suffer 3 strain to use this talent to reduce the damage of the hit by two plus their ranks in Reflect. This talent can only be used once per hit, and your character needs to be wielding a Shield deemed appropriate by the GM.</p>",
+        Source: 'FCRB'
+    },
+    {
+        Name: 'Second Wind',
+        Tier: 1,
+        Activation: 'Active (Incidental)',
+        Ranked: 'Yes',
+        Tags: ['Tough'],
+        Text: "<p><em>You may not take this talent if you have taken the Heroic Recovery talent.</em></p><p>Once per encounter, your character may use this talent to heal an amount of strain equal to their ranks in Second Wind.</p>",
         Source: 'GCRB'
     },
     {

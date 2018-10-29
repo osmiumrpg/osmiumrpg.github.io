9427108d99be5594be9df09e297aa04850cbd2b4
Parker Emerson <parker.emerson@gmail.com>
2018-10-29 15:01:32 -0700

    Typo fixes

--- content/javascript/talents.js
+++ content/javascript/talents.js
@@ -1156,7 +1156,7 @@ var talent_list = [
         Tier: 3,
         Activation: 'Passive',
         Ranked: 'Yes',
-        Tags: ['Beasts']
+        Tags: ['Beasts'],
         Text: "<p>Your character creates a bond with a single animal approved by your GM. This animal must be silhouette 0 (no larger than a mid-sized dog). The bond persists as long as your character chooses, although at your GM’s discretion, the bond may also be broken due to abusive treatment or other extenuating circumstances. As long as the bond persists, the animal follows your character, and you dictate the animal’s overall behavior (although, since the animal is only bonded with the character, not dominated, it may still perform inconvenient actions such as scratching furniture, consuming rations, and marking territory).</p><p>Once per round in structured encounters, your character may spend one maneuver to direct their animal in performing one action and one maneuver during your character’s turn. The animal must be within hearing and visual range of your character (generally medium range) to do this. Otherwise, the animal does not contribute to the encounter. The specifics of its behavior are up to you and your GM.</p><p>For every additional rank of Animal Companion your character has, increase the allowed silhouette of the companion by one (this may mean your character gets a new companion, or their companion grows in size). This talent can also change in flavor depending on the nature of your game setting. While an animal companion may make sense in many settings, in a futuristic setting it may make more sense for the “animal” to be a robot or drone, for example.</p>",
         Source: 'GCRB'
     },
